a25ee9d9a54dccdd4d856381ebcde681f944987b
mbnq <mbnq00@gmail.com>
2024-06-21 17:15:55 +0200

    0.04
    - working version

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Discordiffy
- Drag'n'Drop video file to automatically convert it below discord 25MB limit
+ 	This simple script converts video file until it gets under 24.80MB 
+ 	to fit into free Discord video upload size limit.
+	Simply drag'n'drop video file onto script file.
+ 	This script uses ffmpeg.exe, you can find it on the web.
+
--- discordiffy.bat
+++ discordiffy.bat
@@ -1,51 +1,96 @@
 :: mbnq00@gmail.com
 :: https://www.mbnq.pl/
 
+REM 	This simple script converts video file until it gets under 24.80MB 
+REM 	to fit into free Discord video upload size limit.
+REM		Simply drag'n'drop video file onto script file.
+REM 	This script uses ffmpeg.exe, you can find it on the web.
+
 @echo off
 setlocal enabledelayedexpansion
 title Discordiffy by mbnq.pl
 color 09
 
-set "consoleCaret=> "
+set "cc=> "
 set "ffmpegPath=bin\ffmpeg.exe"
 
+cls
+
+call :intro
 
 if not exist !ffmpegPath! (
-    echo !consoleCaret!FFmpeg is not installed or not in system path.
-    echo !consoleCaret!Please install FFmpeg and adjust it path in the script file.
+    echo !cc!FFmpeg is not installed or not in system path.
+    echo !cc!Please install FFmpeg and adjust it path in the script file.
     goto fail
 )
 
-:: Prompt for file if not provided as argument
 if "%~1"=="" (
-    echo !consoleCaret!Please drag and drop a video file onto the script or pass it as a command line argument.
-    set /p input="!consoleCaret!Enter file path: "
+    echo !cc!Please drag and drop a video file onto the script or pass it as a command line argument.
+    set /p input="!cc!Enter file path: "
 ) else (
     set "input=%~1"
 )
 
-:: Output file setup
-set "output=!~dpn1_converted.mp4"
-
-:: Conversion process
-echo Converting file. Please wait...
-bin\ffmpeg -i "!input!" -b:v 2M -bufsize 2M -maxrate 2M -vf "scale=-2:1080" -c:a aac -b:a 192k "!output!"
-
-:: Check if the output file exceeds the desired maximum file size of 24.80 MB
-for %%I in ("!output!") do set /a size=%%~zI/1048576
-if !size! gtr 24 (
-    echo File size exceeded 24.80 MB, adjusting bitrate.
-    bin\ffmpeg -i "!input!" -fs 24M -c:v libx264 -crf 23 -preset veryslow -c:a aac -b:a 192k "!output!"
+if not exist "!input!" (
+	echo !cc! could not access !input! file.
+	goto fail
 )
 
-echo Conversion complete. Output file is located at: !output!
-pause
-endlocal
+for %%I in ("!input!") do set /a input_size=%%~zI
+if !input_size! leq 25963520 (
+    echo !cc!Input file is already under 25MB.
+	goto fail
+)
+
+set "output=%~dpn1_discordiffied.mp4"
+if exist !output! del /Q /f !output!
+
+echo !cc!Converting file. Please wait...
+bin\ffmpeg -loglevel warning -stats -i "!input!" -b:v 2M -bufsize 2M -maxrate 2M -vf "scale=-2:1080" -c:a aac -b:a 192k "!output!"
+
+if %ERRORLEVEL% neq 0 (
+	echo !cc!Something went wrong...
+	goto fail
+)
+
+for %%I in ("!output!") do set /a size=%%~zI/1048576
+if !size! gtr 24 (
+    echo !cc!File size exceeded 24.80 MB, adjusting bitrate.
+    bin\ffmpeg -y -loglevel warning -stats -i "!input!" -fs 24M -c:v libx264 -crf 23 -preset veryslow -c:a aac -b:a 192k "!output!"
+)
+
+if %ERRORLEVEL% neq 0 (
+	echo !cc!Something went wrong...
+	goto fail
+)
+
+goto bye
 
 :fail
-pause
+echo !cc!Press any key to exit...
+pause > nul
+endlocal
 exit /b 1
 
 :bye
-pause
+echo.
+echo !cc!Conversion complete!
+echo !cc!Output file is located at: 
+echo !cc!!output!
+echo.
+echo !cc!Press any key to exit...
+pause > nul
+endlocal
 exit /b 0
+
+:intro
+	cls
+	echo *********************************************************
+	echo *                                                       *
+	echo *                   Discordiffy                         *
+	echo *                                                       *
+	echo *                                        mbnq.pl 2024   *
+	echo *                                                       *
+	echo *********************************************************
+	echo.
+	exit /b
